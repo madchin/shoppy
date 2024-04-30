0a6be4c80ec7931616adb08689c18b442e965a72
wojesky <m.lojewski1998@gmail.com>
2024-04-30 19:51:00 +0200

    fix returning one error message when two updates could fail

--- backend/handlers/user.go
+++ backend/handlers/user.go
@@ -4,6 +4,7 @@ import (
 	"backend/data"
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 type userService interface {
@@ -38,10 +39,12 @@ func create(userService userService, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	msg, err := json.Marshal(user)
+	//branch not handled in tests, any possibility to occur? need to mock?
 	if err != nil {
 		ErrorMsg(w, http.StatusInternalServerError, GenericError.Message)
 		return
 	}
+	w.WriteHeader(http.StatusCreated)
 	w.Write(msg)
 }
 
@@ -60,17 +63,36 @@ func update(userService userService, uuid string, w http.ResponseWriter, r *http
 		ErrorMsg(w, http.StatusBadRequest, "User with provided id not exists")
 		return
 	}
-	if user.Name != incomingUser.Name {
-		if err = userService.UpdateName(user); err != nil {
-			ErrorMsg(w, http.StatusBadRequest, "Updating name failed")
-			return
+
+	errorChannel := make(chan string, 2)
+	go func(chan<- string) {
+		if user.Name != incomingUser.Name {
+			if err = userService.UpdateName(user); err != nil {
+				errorChannel <- "Updating name failed"
+				return
+			}
+			errorChannel <- ""
+		}
+	}(errorChannel)
+	go func(chan<- string) {
+		if user.Email != incomingUser.Email {
+			if err = userService.UpdateEmail(user); err != nil {
+				errorChannel <- "Updating email failed"
+				return
+			}
+			errorChannel <- ""
+		}
+	}(errorChannel)
+	var errorMessages []string
+	for i := 0; i < 2; i++ {
+		msg := <-errorChannel
+		if msg != "" {
+			errorMessages = append(errorMessages, msg)
 		}
 	}
-	if user.Email != incomingUser.Email {
-		if err = userService.UpdateEmail(user); err != nil {
-			ErrorMsg(w, http.StatusBadRequest, "Updating email failed")
-			return
-		}
+	if len(errorMessages) > 0 {
+		ErrorMsg(w, http.StatusBadRequest, strings.Join(errorMessages, ", "))
+		return
 	}
 	msg, err := json.Marshal(user)
 	if err != nil {
