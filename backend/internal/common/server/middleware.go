be979ea0e2334dec29977efa1c7c1c252ec5472c
wojesky <m.lojewski1998@gmail.com>
2024-06-18 12:48:30 +0200

    fix superflous responses

--- backend/internal/common/server/middleware.go
+++ backend/internal/common/server/middleware.go
@@ -42,27 +42,28 @@ func AuthMiddleware(auth Auth) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// if middleware is not required, omit this middleware
-			if r.Context().Value(BearerAuthScopes) != nil {
-				authHeader := r.Header.Get("Authorization")
-				if authHeader == "" {
-					ErrorHandler(w, r, custom_error.NewAuthorizationError("Unauthorized", "missing 'Authorization' header"))
-					return
-				}
-				authHeaderContent := strings.Split(authHeader, " ")
-				if len(authHeaderContent) != 2 || authHeaderContent[0] != "Bearer" {
-					ErrorHandler(w, r, custom_error.NewAuthorizationError("Unauthorized", "wrong 'Authorization' header format"))
-					return
-				}
-				token := authHeaderContent[1]
-				userInfo, err := auth.Verify(token)
-				if err != nil {
-					ErrorHandler(w, r, custom_error.NewAuthorizationError("Unauthorized", fmt.Sprintf("authorization token is wrong or expired %s", err.Error())))
-					return
-				}
-				ctx := NewContext(r.Context(), userInfo)
-
-				next.ServeHTTP(w, r.WithContext(ctx))
+			if r.Context().Value(BearerAuthScopes) == nil {
+				next.ServeHTTP(w, r)
+				return
 			}
+			authHeader := r.Header.Get("Authorization")
+			if authHeader == "" {
+				ErrorHandler(w, r, custom_error.NewAuthorizationError("Unauthorized", "missing 'Authorization' header"))
+				return
+			}
+			authHeaderContent := strings.Split(authHeader, " ")
+			if len(authHeaderContent) != 2 || authHeaderContent[0] != "Bearer" {
+				ErrorHandler(w, r, custom_error.NewAuthorizationError("Unauthorized", "wrong 'Authorization' header format"))
+				return
+			}
+			token := authHeaderContent[1]
+			userInfo, err := auth.Verify(token)
+			if err != nil {
+				ErrorHandler(w, r, custom_error.NewAuthorizationError("Unauthorized", fmt.Sprintf("authorization token is wrong or expired %s", err.Error())))
+				return
+			}
+			ctx := NewContext(r.Context(), userInfo)
+			r = r.WithContext(ctx)
 			next.ServeHTTP(w, r)
 		}))
 	}
